baa4202e2d81223fd27121fd7484566acff7e54e
Leo Truchet <leo.truchet@edhec.com>
2022-06-06 11:52:59 +0200

    init basics

---
+++ 00-Ruby/01-Basics/02-Age-In-Days/lib/age_in_days.rb
@@ -0,0 +1,8 @@
+# This "require" line loads the contents of the 'date' file from the standard
+# Ruby library, giving you access to the Date class.
+require 'date'
+
+def age_in_days(day, month, year)
+  # return "You are #{age.to_s} days old"
+  # TODO: return the age expressed in days given the day, month, and year of birth
+end
