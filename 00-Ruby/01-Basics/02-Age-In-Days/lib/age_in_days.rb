6bcfaf6e87ee103479877b0fc84f4abe50b78ee4
Leo Truchet <leo.truchet@edhec.com>
2022-06-06 19:28:38 +0200

    Exo 02: Done

--- 00-Ruby/01-Basics/02-Age-In-Days/lib/age_in_days.rb
+++ 00-Ruby/01-Basics/02-Age-In-Days/lib/age_in_days.rb
@@ -5,4 +5,10 @@ require 'date'
 def age_in_days(day, month, year)
   # return "You are #{age.to_s} days old"
   # TODO: return the age expressed in days given the day, month, and year of birth
+
+  # date d'aujd - date de naissance
+  nb_jours = Date.today - Date.new(year, month, day)
+
+  return nb_jours.to_i
+
 end
